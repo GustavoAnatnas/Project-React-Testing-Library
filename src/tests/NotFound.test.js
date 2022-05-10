74151d595f90c9574f87c5280c6e2739cecac100
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-05-10 04:05:00 -0300

    Desafio 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Desafio 4', () => {
+  test('Teste se a página contém um heading h2', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-desconhecida');
+
+    const heading = screen.getByText((content) => content.startsWith('Page requested'));
+    expect(heading).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-desconhecida');
+
+    const gifNotFoundLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const gifNotFound = screen.getByRole('img', { name: /pikachu/i });
+    expect(gifNotFound).toBeInTheDocument();
+    expect(gifNotFound.src).toContain(gifNotFoundLink);
+  });
+});
